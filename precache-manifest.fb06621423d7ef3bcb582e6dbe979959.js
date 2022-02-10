7f851db394949b85f49896494e7fb729b7bf3b22
sorokinaolga <sorokinaop@list.ru>
2022-02-10 15:56:21 +0300

    Updates

---
+++ precache-manifest.fb06621423d7ef3bcb582e6dbe979959.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcab0a0ac1e3dfffde37877e91f7ff8",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "6dcefab1d624bbfe48b8",
+    "url": "/react-shop/static/css/2.616459d7.chunk.css"
+  },
+  {
+    "revision": "de0827e138d3d4c814ca",
+    "url": "/react-shop/static/css/main.5fd01228.chunk.css"
+  },
+  {
+    "revision": "6dcefab1d624bbfe48b8",
+    "url": "/react-shop/static/js/2.81b1e7f8.chunk.js"
+  },
+  {
+    "revision": "de0827e138d3d4c814ca",
+    "url": "/react-shop/static/js/main.d1ff53bd.chunk.js"
+  },
+  {
+    "revision": "fe6f9fd39bb3c7c84741",
+    "url": "/react-shop/static/js/runtime-main.d6bbc8b7.js"
+  },
+  {
+    "revision": "889f15f3395d52a4c48400674336e60d",
+    "url": "/react-shop/static/media/error.889f15f3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b0b3e5f11e034853073aaed608fdd94.js"
+  "/react-shop/precache-manifest.fb06621423d7ef3bcb582e6dbe979959.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-shop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
