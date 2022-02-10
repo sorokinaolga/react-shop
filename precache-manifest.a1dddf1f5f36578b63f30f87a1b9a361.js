b6a9cfe7efdd4301d473c9720f081e7e6216a41e
sorokinaolga <sorokinaop@list.ru>
2022-02-10 15:28:30 +0300

    Updates

---
+++ precache-manifest.a1dddf1f5f36578b63f30f87a1b9a361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3a423d0c1b1af29bbba32216da0ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcefab1d624bbfe48b8",
+    "url": "/static/css/2.616459d7.chunk.css"
+  },
+  {
+    "revision": "c13307f16efe5a967b77",
+    "url": "/static/css/main.5fd01228.chunk.css"
+  },
+  {
+    "revision": "6dcefab1d624bbfe48b8",
+    "url": "/static/js/2.81b1e7f8.chunk.js"
+  },
+  {
+    "revision": "c13307f16efe5a967b77",
+    "url": "/static/js/main.6213915d.chunk.js"
+  },
+  {
+    "revision": "a4c7d18271b1f6b3fbe9",
+    "url": "/static/js/runtime-main.abbf2941.js"
+  },
+  {
+    "revision": "889f15f3395d52a4c48400674336e60d",
+    "url": "/static/media/error.889f15f3.png"
+  }
+]);
